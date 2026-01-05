e05edca203d1e8c5cbf6ed79df5288c973271620
Nordkuma <nordkuma@proton.me>
2026-01-05 21:44:07 +0900

    Update dot_config/nvim

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -3,13 +3,15 @@ vim.g.maplocalleader = '\\'
 
 local map = vim.keymap.set
 
-map('', '<Left>', '0')
-map('', '<Right>', '$')
+map('', '<Down>', '}')
+map('', '<Left>', '^')
+map('', '<Right>', 'g_')
+map('', '<Up>', '{')
 
 map({ 'n', 'i', 'v', 'c' }, '<C-Space>', '<Esc>', { silent = true })
 
 map('n', 'x', '"_x')
 map('n', '*', [[:let @/ = '\<' . expand('<cword>') . '\>'<CR>:set hlsearch<CR>]], { silent = true })
 map('n', '<Esc><Esc>', ':nohlsearch<CR>', { silent = true })
-map('n', '<Leader>o', 'm`o<Esc>``', { silent = true })
-map('n', '<Leader>O', 'm`O<Esc>``', { silent = true })
+map('n', '<Leader>o', 'o<Esc>', { silent = true })
+map('n', '<Leader>O', 'O<Esc>', { silent = true })
