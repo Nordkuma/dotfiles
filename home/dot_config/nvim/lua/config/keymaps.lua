da8dcfebcef0216565b412031618a9a6182ee8ec
Nordkuma <nordkuma@proton.me>
2026-03-08 22:00:36 +0900

    Update dot_config/nvim

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -8,14 +8,15 @@ map('', '<Left>', '^')
 map('', '<Right>', '$')
 map('', '<Up>', '{')
 
-map({ 'n', 'i', 'v', 'c' }, '<C-Space>', '<Esc>', { silent = true })
+map({ 'n', 'i', 'v', 'c' }, '<C-Space>', '<Esc>')
 
 map('n', 'c', '"_c')
 map('n', 'x', '"_x')
 map('n', '*', [[:let @/ = '\<' . expand('<cword>') . '\>'<CR>:set hlsearch<CR>]], { silent = true })
 map('n', '<Leader>l', ':nohlsearch<CR>', { silent = true })
-map('n', '<Leader>o', 'o<Esc>', { silent = true })
-map('n', '<Leader>O', 'O<Esc>', { silent = true })
+map('n', '<Leader>o', 'o<Esc>')
+map('n', '<Leader>O', 'O<Esc>')
+map('n', '<Right>', function() vim.fn.setcharpos('.', { 0, vim.fn.line('.'), vim.fn.charcol('$'), 0, vim.v.maxcol }) end)
 
 map('v', '<', '<gv')
 map('v', '>', '>gv')
