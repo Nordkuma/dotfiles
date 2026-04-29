c1ab43d14440031d5ac7a41d957cb93110a7f10a
Nordkuma <nordkuma@proton.me>
2026-04-29 16:54:09 +0900

    Update dot_config/nvim

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -22,9 +22,11 @@ if vim.fn.executable('ime') == 1 then
         return '<Esc>'
     end, { expr = true })
 end
-map('n', '<Leader>l', ':nohlsearch<CR>', { silent = true })
+map('n', '<Leader>l', '<cmd>nohlsearch<CR>', { silent = true })
 map('n', '<Leader>o', 'o<Esc>')
 map('n', '<Leader>O', 'O<Esc>')
+map('n', '<leader>w', '<cmd>w<CR>')
+map('n', '<leader>x', '<cmd>x<CR>')
 map('n', '<Right>', function() vim.fn.setcharpos('.', { 0, vim.fn.line('.'), vim.fn.charcol('$'), 0, vim.v.maxcol }) end)
 
 map('v', '<', '<gv')
